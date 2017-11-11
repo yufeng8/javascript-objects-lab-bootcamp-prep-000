752971c00037351e813207186e362be1fdf90873
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 08:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var recipes = [stew beef: "beef"]
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var object = {prop:1}
-  object[prop2]=2
-  //Object.assign({}, object, {key:value})
+  object[key]=value
+  return Object.assign({}, object, {key:value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key
+  return 
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return 
+}
