8361095c396b61e634076126caaa61ffd7ea9f68
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 08:58:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return
 }
 
